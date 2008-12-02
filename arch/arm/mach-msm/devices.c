ece37f47dd06312e8eec9ed16ba945bb81319481
Nick Pelly <npelly@google.com>
2008-12-01 22:41:17 -0800

    [ARM] msm_serial_hs: Redo hs uart driver for MSM7k
    - Do not use workqueues
    - Use dma_sync instead of dma_map in rx and tx callbacks
    - Fix bug where last RX is repeated into tty on shutdown
    - Fix membase / mapbase mix-up
    - Fix shutdown and remove paths. Tested as module
    - UART2 now works
    - Use container_of and correct types instead of casts
    - Use msm_hs_ prefix for local functions and structs
    - Remove the non-static pm interface
    - Acquire uart spinlock in irq handler, dma callback and set_termios
    - Use waitqueue instead of completion
    - Use get_resource helpers
    - Remove orphanned Kconfig options
    - Use new clockname/device node key for clocks
    - Plenty more misc cleanup
    
    Signed-off-by: Nick Pelly <npelly@google.com>

--- arch/arm/mach-msm/devices.c
+++ arch/arm/mach-msm/devices.c
@@ -15,6 +15,7 @@
 
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
+#include <linux/dma-mapping.h>
 
 #include <mach/irqs.h>
 #include <mach/msm_iomap.h>
@@ -101,7 +102,7 @@ static struct resource msm_uart1_dm_resources[] = {
 	{
 		.start = DMOV_HSUART1_TX_CHAN,
 		.end   = DMOV_HSUART1_RX_CHAN,
-		.name  = "uartdm_chanels",
+		.name  = "uartdm_channels",
 		.flags = IORESOURCE_DMA,
 	},
 	{
@@ -112,11 +113,17 @@ static struct resource msm_uart1_dm_resources[] = {
 	},
 };
 
+static u64 msm_uart_dm1_dma_mask = DMA_BIT_MASK(32);
+
 struct platform_device msm_device_uart_dm1 = {
 	.name = "msm_serial_hs",
-	.id = 1,
+	.id = 0,
 	.num_resources = ARRAY_SIZE(msm_uart1_dm_resources),
 	.resource = msm_uart1_dm_resources,
+	.dev		= {
+		.dma_mask = &msm_uart_dm1_dma_mask,
+		.coherent_dma_mask = DMA_BIT_MASK(32),
+	},
 };
 
 static struct resource msm_uart2_dm_resources[] = {
@@ -133,7 +140,7 @@ static struct resource msm_uart2_dm_resources[] = {
 	{
 		.start = DMOV_HSUART2_TX_CHAN,
 		.end   = DMOV_HSUART2_RX_CHAN,
-		.name  = "uartdm_chanels",
+		.name  = "uartdm_channels",
 		.flags = IORESOURCE_DMA,
 	},
 	{
@@ -144,11 +151,17 @@ static struct resource msm_uart2_dm_resources[] = {
 	},
 };
 
+static u64 msm_uart_dm2_dma_mask = DMA_BIT_MASK(32);
+
 struct platform_device msm_device_uart_dm2 = {
 	.name = "msm_serial_hs",
-	.id = 2,
+	.id = 1,
 	.num_resources = ARRAY_SIZE(msm_uart2_dm_resources),
 	.resource = msm_uart2_dm_resources,
+	.dev		= {
+		.dma_mask = &msm_uart_dm2_dma_mask,
+		.coherent_dma_mask = DMA_BIT_MASK(32),
+	},
 };
 
 static struct resource resources_i2c[] = {
