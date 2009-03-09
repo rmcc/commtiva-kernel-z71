94e7a0ce58d46cd7b2e03d73043481041916988c
Mike Chan <mike@android.com>
2009-03-09 15:48:41 -0700

    [ARM] msm: rmnet: Different stat timeouts when screen is on/off.
    Timeout for modem powerdown can differ when the screen is on/off.
    Allow timeout to change via early suspend/resume hooks.
    
    Signed-off-by: Mike Chan <mike@android.com>

--- drivers/net/msm_rmnet.c
+++ drivers/net/msm_rmnet.c
@@ -27,6 +27,10 @@
 #include <linux/etherdevice.h>
 #include <linux/skbuff.h>
 
+#ifdef CONFIG_HAS_EARLYSUSPEND
+#include <linux/earlysuspend.h>
+#endif
+
 #include <mach/msm_smd.h>
 
 /* XXX should come from smd headers */
@@ -56,6 +60,60 @@ static int count_this_packet(void *_hdr, int len)
 }
 
 #ifdef CONFIG_MSM_RMNET_DEBUG
+static unsigned long timeout_us;
+
+#ifdef CONFIG_HAS_EARLYSUSPEND
+/*
+ * If early suspend is enabled then we specify two timeout values,
+ * screen on (default), and screen is off.
+ */
+static unsigned long timeout_suspend_us;
+static struct device *rmnet0;
+
+/* Set timeout in us when the screen is off. */
+static ssize_t timeout_suspend_store(struct device *d, struct device_attribute *attr, const char *buf, size_t n)
+{
+	timeout_suspend_us = simple_strtoul(buf, NULL, 10);
+	return n;
+}
+
+static ssize_t timeout_suspend_show(struct device *d,
+				    struct device_attribute *attr,
+				    char *buf)
+{
+	return sprintf(buf, "%lu\n", (unsigned long) timeout_suspend_us);
+}
+
+static DEVICE_ATTR(timeout_suspend, 0664, timeout_suspend_show, timeout_suspend_store);
+
+static void rmnet_early_suspend(struct early_suspend *handler) {
+	if (rmnet0) {
+		struct rmnet_private *p = netdev_priv(to_net_dev(rmnet0));
+		p->timeout_us = timeout_suspend_us;
+	}
+}
+
+static void rmnet_late_resume(struct early_suspend *handler) {
+	if (rmnet0) {
+		struct rmnet_private *p = netdev_priv(to_net_dev(rmnet0));
+		p->timeout_us = timeout_us;
+	}
+}
+
+static struct early_suspend rmnet_power_suspend = {
+	.suspend = rmnet_early_suspend,
+	.resume = rmnet_late_resume,
+};
+
+static int __init rmnet_late_init(void)
+{
+	register_early_suspend(&rmnet_power_suspend);
+	return 0;
+}
+
+late_initcall(rmnet_late_init);
+#endif
+
 /* Returns 1 if packet caused rmnet to wakeup, 0 otherwise. */
 static int rmnet_cause_wakeup(struct rmnet_private *p) {
 	int ret = 0;
@@ -96,17 +154,22 @@ DEVICE_ATTR(wakeups_rcv, 0444, wakeups_rcv_show, NULL);
 static ssize_t timeout_store(struct device *d, struct device_attribute *attr,
 		const char *buf, size_t n)
 {
+#ifndef CONFIG_HAS_EARLYSUSPEND
 	struct rmnet_private *p = netdev_priv(to_net_dev(d));
-	p->timeout_us = simple_strtoul(buf, NULL, 10);
+	p->timeout_us = timeout_us = simple_strtoul(buf, NULL, 10);
+#else
+/* If using early suspend/resume hooks do not write the value on store. */
+	timeout_us = simple_strtoul(buf, NULL, 10);
+#endif
 	return n;
 }
 
 static ssize_t timeout_show(struct device *d, struct device_attribute *attr,
-		const char *buf, size_t n)
+			    char *buf)
 {
 	struct rmnet_private *p = netdev_priv(to_net_dev(d));
 	p = netdev_priv(to_net_dev(d));
-	return sprintf(buf, "%lu\n", p->timeout_us);
+	return sprintf(buf, "%lu\n", timeout_us);
 }
 
 DEVICE_ATTR(timeout, 0664, timeout_show, timeout_store);
@@ -266,6 +329,13 @@ static int __init rmnet_init(void)
 	struct rmnet_private *p;
 	unsigned n;
 
+#ifdef CONFIG_MSM_RMNET_DEBUG
+	timeout_us = 0;
+#ifdef CONFIG_HAS_EARLYSUSPEND
+	timeout_suspend_us = 0;
+#endif
+#endif
+
 	for (n = 0; n < 3; n++) {
 		dev = alloc_netdev(sizeof(struct rmnet_private),
 				   "rmnet%d", rmnet_setup);
@@ -277,7 +347,7 @@ static int __init rmnet_init(void)
 		p = netdev_priv(dev);
 		p->chname = ch_name[n];
 #ifdef CONFIG_MSM_RMNET_DEBUG
-		p->timeout_us = 0;
+		p->timeout_us = timeout_us;
 		p->wakeups_xmit = p->wakeups_rcv = 0;
 #endif
 
@@ -288,13 +358,23 @@ static int __init rmnet_init(void)
 		}
 
 #ifdef CONFIG_MSM_RMNET_DEBUG
-		device_create_file(d, &dev_attr_timeout);
-		device_create_file(d, &dev_attr_wakeups_xmit);
-		device_create_file(d, &dev_attr_wakeups_rcv);
+		if (device_create_file(d, &dev_attr_timeout))
+			continue;
+		if (device_create_file(d, &dev_attr_wakeups_xmit))
+			continue;
+		if (device_create_file(d, &dev_attr_wakeups_rcv))
+			continue;
+#ifdef CONFIG_HAS_EARLYSUSPEND
+		if (device_create_file(d, &dev_attr_timeout_suspend))
+			continue;
+
+		/* Only care about rmnet0 for suspend/resume tiemout hooks. */
+		if (n == 0)
+			rmnet0 = d;
+#endif
 #endif
 	}
 	return 0;
 }
-
 
 module_init(rmnet_init);
