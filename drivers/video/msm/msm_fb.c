0e162d95f186ddc30a0869c28ccc0663bcb2d9ff
Neil Leeder <nleeder@quicinc.com>
2009-07-01 13:35:07 -0400

    [ARM] msm fb: only call client_id function when it's defined
    mddi_get_client_id is only defined when MDDI autodetect is
    configured, so only call it when correct config option is set
    
    Signed-off-by: Neil Leeder <nleeder@quicinc.com>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -173,9 +173,11 @@ static struct msm_fb_platform_data *msm_fb_pdata;
 int msm_fb_detect_client(const char *name)
 {
 	int ret = -EPERM;
+#ifdef CONFIG_FB_MSM_MDDI_AUTO_DETECT
 	u32 id;
 
 	id = mddi_get_client_id();
+#endif
 
 	if (msm_fb_pdata && msm_fb_pdata->detect_client)
 		ret = msm_fb_pdata->detect_client(name);
