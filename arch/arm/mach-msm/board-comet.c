90edbdd4b517afd00a3b85edc818eb1a0a32779c
Neil Leeder <nleeder@quicinc.com>
2009-07-23 13:46:13 -0400

    [ARM] msm: use group operations for SPI GPIO config in comet
    Uses the new gpio operations functions to reduce code size
    in the comet board file.
    
    Signed-off-by: Neil Leeder <nleeder@quicinc.com>

--- arch/arm/mach-msm/board-comet.c
+++ arch/arm/mach-msm/board-comet.c
@@ -468,44 +468,26 @@ static struct spi_board_info msm_spi_board_info[] __initdata = {
 	}
 };
 
-static unsigned qsd_spi_gpio_config_data[] = {
-	GPIO_CFG(17,  1, GPIO_INPUT,  GPIO_NO_PULL, GPIO_2MA),  /* SPI_CLK */
-	GPIO_CFG(18,  1, GPIO_INPUT,  GPIO_NO_PULL, GPIO_2MA),  /* SPI_MOSI */
-	GPIO_CFG(19,  1, GPIO_INPUT,  GPIO_NO_PULL, GPIO_2MA),  /* SPI_MISO */
-	GPIO_CFG(20,  1, GPIO_INPUT,  GPIO_NO_PULL, GPIO_2MA),  /* SPI_CS0 */
+static struct msm_gpio qsd_spi_gpio_config_data[] = {
+	{ GPIO_CFG(17,  1, GPIO_INPUT,  GPIO_NO_PULL, GPIO_2MA), "spi_clk" },
+	{ GPIO_CFG(18,  1, GPIO_INPUT,  GPIO_NO_PULL, GPIO_2MA), "spi_mosi" },
+	{ GPIO_CFG(19,  1, GPIO_INPUT,  GPIO_NO_PULL, GPIO_2MA), "spi_miso" },
+	{ GPIO_CFG(20,  1, GPIO_INPUT,  GPIO_NO_PULL, GPIO_2MA), "spi_cs0" },
 };
 
 static int msm_qsd_spi_gpio_config(void)
 {
-	int i, rc;
+	int rc;
 
-	if (gpio_request(17, "spi_clk"))
-		pr_err("failed to request gpio spi_clk\n");
-	if (gpio_request(18, "spi_mosi"))
-		pr_err("failed to request gpio spi_mosi\n");
-	if (gpio_request(19, "spi_miso"))
-		pr_err("failed to request gpio spi_miso\n");
-	if (gpio_request(20, "spi_cs0"))
-		pr_err("failed to request gpio spi_cs0\n");
-
-	for (i = 0; i < ARRAY_SIZE(qsd_spi_gpio_config_data); i++) {
-		rc = gpio_tlmm_config(qsd_spi_gpio_config_data[i], GPIO_ENABLE);
-		if (rc) {
-			printk(KERN_ERR "%s: gpio_tlmm_config(%#x)=%d\n",
-				__func__, qsd_spi_gpio_config_data[i], rc);
-			return -EIO;
-		}
-	}
-
-	return 0;
+	rc = msm_gpios_request_enable(qsd_spi_gpio_config_data,
+		ARRAY_SIZE(qsd_spi_gpio_config_data));
+	return rc;
 }
 
 static void msm_qsd_spi_gpio_release(void)
 {
-	gpio_free(17);
-	gpio_free(18);
-	gpio_free(19);
-	gpio_free(20);
+	msm_gpios_disable_free(qsd_spi_gpio_config_data,
+		ARRAY_SIZE(qsd_spi_gpio_config_data));
 }
 
 static struct msm_spi_platform_data qsd_spi_pdata = {
