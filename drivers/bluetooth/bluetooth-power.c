2cfa2ec9d70c9e571940481642045d367f9773ec
Wilson, Matt <mtwilson@quicinc.com>
2009-08-29 00:07:48 -0500

    bluetooth: support only rfkill for power switch
    This change removes the module parameter interface.
    Android (and BlueZ 4) use only rfkill to switch BT.
    Removal of original interface removes spinlocks that
    prevent operation with Marimba core driver mutex.
    
    Signed-off-by: Matthew Wilson <mtwilson@quicinc.com>

--- drivers/bluetooth/bluetooth-power.c
+++ drivers/bluetooth/bluetooth-power.c
@@ -62,24 +62,17 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/moduleparam.h>
 #include <linux/kernel.h>
-#include <linux/spinlock.h>
 #include <linux/platform_device.h>
 #include <linux/rfkill.h>
-
-static int bluetooth_power_state;
-static int (*power_control)(int enable);
-
-static DEFINE_SPINLOCK(bt_power_lock);
 
 static int bluetooth_toggle_radio(void *data, enum rfkill_state state)
 {
 	int ret;
+	int (*power_control)(int enable);
 
-	spin_lock(&bt_power_lock);
+	power_control = data;
 	ret = (*power_control)((state == RFKILL_STATE_UNBLOCKED) ? 1 : 0);
-	spin_unlock(&bt_power_lock);
 	return ret;
 }
 
@@ -107,6 +100,7 @@ static int bluetooth_power_rfkill_probe(struct platform_device *pdev)
 
 	rfkill->name = "bt_power";
 	rfkill->toggle_radio = bluetooth_toggle_radio;
+	rfkill->data = pdev->dev.platform_data;
 	ret = rfkill_register(rfkill);
 	if (ret) {
 		printk(KERN_DEBUG
@@ -132,38 +126,6 @@ static void bluetooth_power_rfkill_remove(struct platform_device *pdev)
 	platform_set_drvdata(pdev, NULL);
 }
 
-static int bluetooth_power_param_set(const char *val, struct kernel_param *kp)
-{
-	int ret;
-
-	printk(KERN_DEBUG
-		"%s: previous power_state=%d\n",
-		__func__, bluetooth_power_state);
-
-	/* lock change of state and reference */
-	spin_lock(&bt_power_lock);
-	ret = param_set_bool(val, kp);
-	if (power_control) {
-		if (!ret)
-			ret = (*power_control)(bluetooth_power_state);
-		else
-			printk(KERN_ERR "%s param set bool failed (%d)\n",
-					__func__, ret);
-	} else {
-		printk(KERN_INFO
-			"%s: deferring power switch until probe\n",
-			__func__);
-	}
-	spin_unlock(&bt_power_lock);
-	printk(KERN_INFO
-		"%s: current power_state=%d\n",
-		__func__, bluetooth_power_state);
-	return ret;
-}
-
-module_param_call(power, bluetooth_power_param_set, param_get_bool,
-		  &bluetooth_power_state, S_IWUSR | S_IRUGO);
-
 static int __init bt_power_probe(struct platform_device *pdev)
 {
 	int ret = 0;
@@ -176,44 +138,18 @@ static int __init bt_power_probe(struct platform_device *pdev)
 		return -ENOSYS;
 	}
 
-	spin_lock(&bt_power_lock);
-	power_control = pdev->dev.platform_data;
-
-	if (bluetooth_power_state) {
-		printk(KERN_INFO
-			"%s: handling deferred power switch\n",
-			__func__);
-	}
-	ret = (*power_control)(bluetooth_power_state);
-	spin_unlock(&bt_power_lock);
-
-	if (!ret && !bluetooth_power_state &&
-		    bluetooth_power_rfkill_probe(pdev))
-		ret = -ENOMEM;
+	ret = bluetooth_power_rfkill_probe(pdev);
 
 	return ret;
 }
 
 static int __devexit bt_power_remove(struct platform_device *pdev)
 {
-	int ret;
-
 	printk(KERN_DEBUG "%s\n", __func__);
 
 	bluetooth_power_rfkill_remove(pdev);
 
-	if (!power_control) {
-		printk(KERN_ERR "%s: power_control function not initialized\n",
-				__func__);
-		return -ENOSYS;
-	}
-	spin_lock(&bt_power_lock);
-	bluetooth_power_state = 0;
-	ret = (*power_control)(bluetooth_power_state);
-	power_control = NULL;
-	spin_unlock(&bt_power_lock);
-
-	return ret;
+	return 0;
 }
 
 static struct platform_driver bt_power_driver = {
