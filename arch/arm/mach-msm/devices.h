33c31c863be382ed3ab548237591b3fab02422bc
Sahitya Tummala <stummala@qualcomm.com>
2009-09-10 16:06:43 +0530

    msm: Add shared memory resource for remote storage server
    Signed-off-by: Sahitya Tummala <stummala@qualcomm.com>

--- arch/arm/mach-msm/devices.h
+++ arch/arm/mach-msm/devices.h
@@ -31,6 +31,8 @@ extern struct platform_device msm_device_sdc2;
 extern struct platform_device msm_device_sdc3;
 extern struct platform_device msm_device_sdc4;
 
+extern struct platform_device rmt_storage_device;
+
 extern struct platform_device msm_device_hsusb_otg;
 extern struct platform_device msm_device_hsusb_peripheral;
 extern struct platform_device msm_device_gadget_peripheral;
