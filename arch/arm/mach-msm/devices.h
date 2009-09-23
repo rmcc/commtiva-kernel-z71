1e5b040cedbc8d0dde4487a1c61773fef7eede64
Sagar Dharia <sdharia@quicinc.com>
2009-09-23 13:29:15 -0600

    qup_i2c: Add clock, resources entries for QUP i2c mini-core device
    Signed-off-by: Sagar Dharia <sdharia@quicinc.com>

--- arch/arm/mach-msm/devices.h
+++ arch/arm/mach-msm/devices.h
@@ -43,6 +43,8 @@ extern struct platform_device msm_device_i2c;
 
 extern struct platform_device msm_device_i2c_2;
 
+extern struct platform_device qup_device_i2c;
+
 extern struct platform_device msm_device_smd;
 extern struct platform_device msm_device_dmov;
 
