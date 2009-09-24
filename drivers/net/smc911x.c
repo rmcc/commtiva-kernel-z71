6aafdd596e5094554ae994b8e1b273e0f14960d5
Shubha Nagaraja Rao <shubhan@quicinc.com>
2009-09-24 15:17:17 -0400

    smc911x: Powerup phy before software reset
    Manual shutdown and bringup of network interface using
    ifconfig does not powerup the phy causing timeout of
    software reset on Comet2. Add an explicit powerup
    command into the reset sequence.
    
    Signed-off-by: Shubha Nagaraja Rao <shubhan@quicinc.com>

--- drivers/net/smc911x.c
+++ drivers/net/smc911x.c
@@ -173,6 +173,29 @@ static void PRINT_PKT(u_char *buf, int length)
 	SMC_SET_INT_EN((lp), __mask);			\
 } while (0)
 
+static void smc911x_phy_powerup(struct net_device *dev, int phy)
+{
+	struct smc911x_local *lp = netdev_priv(dev);
+	unsigned int bmcr, bmsr;
+	int timeout;
+
+	SMC_GET_PHY_BMCR(lp, phy, bmcr);
+	bmcr &= ~BMCR_PDOWN;
+	SMC_SET_PHY_BMCR(lp, phy, bmcr);
+	if (bmcr & BMCR_ANENABLE) {
+		timeout = 1000;
+		while (--timeout) {
+			SMC_GET_PHY_BMSR(lp, phy, bmsr);
+			if (bmsr & BMSR_ANEGCOMPLETE)
+				break;
+			udelay(10);
+		}
+		/* If timeout hits, it proceeds to software reset and
+		times out there since the phy params are not set due to
+		failure of auto negotiation */
+	}
+}
+
 /*
  * this does a soft reset on the device
  */
@@ -198,6 +221,9 @@ static void smc911x_reset(struct net_device *dev)
 			return;
 		}
 	}
+
+	/* Powerup the phy before the actual reset */
+	smc911x_phy_powerup(dev, lp->mii.phy_id);
 
 	/* Disable all interrupts */
 	spin_lock_irqsave(&lp->lock, flags);
