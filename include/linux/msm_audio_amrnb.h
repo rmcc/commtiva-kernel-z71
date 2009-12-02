e823fc10190bdb996fe11fcb7bef700a73923eee
Rajesha Kini <rkini@qualcomm.com>
2009-12-03 05:13:02 +0530

    msm: audio: 7x30: Add AMRNB encoder driver
    Add AMRNB encoder driver, which provides encoding feature
    from internal codec as well as In Call Encoding.
    Default set to Internal Codec.
    
    Signed-off-by: Rajesha Kini <rkini@qualcomm.com>

--- include/linux/msm_audio_amrnb.h
+++ include/linux/msm_audio_amrnb.h
@@ -25,6 +25,12 @@
 	(AUDIO_MAX_COMMON_IOCTL_NUM+0), unsigned)
 #define AUDIO_SET_AMRNB_ENC_CONFIG  _IOR(AUDIO_IOCTL_MAGIC, \
 	(AUDIO_MAX_COMMON_IOCTL_NUM+1), unsigned)
+#define AUDIO_GET_AMRNB_ENC_CONFIG_V2  _IOW(AUDIO_IOCTL_MAGIC, \
+	(AUDIO_MAX_COMMON_IOCTL_NUM+2), \
+	struct msm_audio_amrnb_enc_config_v2)
+#define AUDIO_SET_AMRNB_ENC_CONFIG_V2  _IOR(AUDIO_IOCTL_MAGIC, \
+	(AUDIO_MAX_COMMON_IOCTL_NUM+3), \
+	struct msm_audio_amrnb_enc_config_v2)
 
 struct msm_audio_amrnb_enc_config {
 	unsigned short voicememoencweight1;
@@ -35,5 +41,11 @@ struct msm_audio_amrnb_enc_config {
 	unsigned short test_mode_enable; /* 0xFFFF - enable, 0- disable */
 	unsigned short enc_mode; /* 0-MR475,1-MR515,2-MR59,3-MR67,4-MR74
 				5-MR795, 6- MR102, 7- MR122(default) */
+};
+
+struct msm_audio_amrnb_enc_config_v2 {
+	uint32_t band_mode;
+	uint32_t dtx_enable;
+	uint32_t frame_format;
 };
 #endif /* __MSM_AUDIO_AMRNB_H */
