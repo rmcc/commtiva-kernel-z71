b75348c2baf8c448b0c85405667d7b1ebafa9741
Asish Bhattacharya <asishb@qualcomm.com>
2009-12-03 13:17:25 +0530

    audio: 7x30: Add listener code to mixer interface
    All decoder/encoder and voice drivers registers to this listener and
    would be informed of any event changes like routing or device
    enable/disable events.
    The drivers are then required to use this to take appropriate action
    
    Signed-off-by: Asish Bhattacharya <asishb@qualcomm.com>

--- arch/arm/mach-msm/include/mach/qdsp5v2/audio_dev_ctl.h
+++ arch/arm/mach-msm/include/mach/qdsp5v2/audio_dev_ctl.h
@@ -29,20 +29,24 @@
 #define __MACH_QDSP5_V2_SNDDEV_H
 #include <mach/qdsp5v2/audio_def.h>
 
+#define AUDIO_DEV_CTL_MAX_DEV 16
+
 struct msm_snddev_info {
 	const char *name;
 	u32 capability;
 	u32 copp_id;
 	u32 acdb_id;
+	u32 dev_volume;
 	struct msm_snddev_ops {
 		int (*open)(struct msm_snddev_info *);
 		int (*close)(struct msm_snddev_info *);
-	  int (*set_freq)(struct msm_snddev_info *, u32);
+		int (*set_freq)(struct msm_snddev_info *, u32);
 	} dev_ops;
 	u8 opened;
 	void *private_data;
 	bool state;
 	u32 sample_rate;
+	u32 sessions;
 };
 
 void msm_snddev_register(struct msm_snddev_info *);
@@ -55,7 +59,87 @@ int msm_snddev_set_dec(int popp_id, int copp_id, int set);
 int msm_snddev_set_enc(int popp_id, int copp_id, int set);
 int msm_snddev_is_set(int popp_id, int copp_id);
 int msm_get_voc_route(u32 *rx_id, u32 *tx_id);
-int msm_set_voc_route(struct msm_snddev_info *dev_info, int stream_type);
+int msm_set_voc_route(struct msm_snddev_info *dev_info, int stream_type,
+			int dev_id);
 struct msm_snddev_info *audio_dev_ctrl_find_dev(u32 dev_id);
+
 void msm_release_voc_thread(void);
+
+int snddev_voice_set_volume(int vol, int path);
+
+struct auddev_evt_voc_devinfo {
+	u32 rxdev_id;
+	u32 txdev_id;
+	u32 rxdev_vol;
+	u32 txdev_vol;
+	u32 rxdev_sample;
+	u32 txdev_sample;
+	u32 rx_mute;
+	u32 tx_mute;
+
+};
+struct auddev_evt_audcal_info {
+	u32 dev_id;
+	u32 acdb_id;
+	u32 sample_rate;
+	u32 dev_type;
+};
+
+union auddev_evt_data {
+	struct auddev_evt_voc_devinfo voc_devinfo;
+	u32 routing_id;
+	struct auddev_evt_audcal_info audcal_info;
+};
+
+struct message_header {
+	uint32_t id;
+	uint32_t data_len;
+};
+
+#define AUDDEV_EVT_DEV_CHG_VOICE	0x01 /* device change event */
+#define AUDDEV_EVT_DEV_CHG_AUDIO	0x02 /* device change event */
+#define AUDDEV_EVT_DEV_RDY 		0x04 /* device ready event */
+#define AUDDEV_EVT_DEV_RLS 		0x08 /* device released event */
+#define AUDDEV_EVT_DEV_CONCURRENT 	0x10 /* device have >1 active session */
+#define AUDDEV_EVT_DEV_NON_CONCURRENT 	0x20 /* device have 1 active session */
+#define AUDDEV_EVT_REL_PENDING		0x40
+#define AUDDEV_EVT_DEVICE_VOL_MUTE_CHG	0x80
+
+#define AUDDEV_CLNT_VOC 0x1
+#define AUDDEV_CLNT_DEC 0x2
+#define AUDDEV_CLNT_ENC 0x3
+#define AUDDEV_CLNT_AUDIOCAL 0x4
+
+#define AUDIO_DEV_CTL_MAX_LISTNER	20
+
+struct msm_snd_evt_listner {
+	uint32_t evt_id; /* TODO remove this if not req*/
+	uint32_t clnt_type;
+	uint32_t clnt_id;
+	void *private_data;
+	void (*auddev_evt_listener)(u32 evt_id,
+		union auddev_evt_data *evt_payload,
+		void *private_data);
+	struct msm_snd_evt_listner *cb_next;
+	struct msm_snd_evt_listner *cb_prev;
+};
+
+struct event_listner {
+	struct mutex listner_lock;
+	struct msm_snd_evt_listner *cb;
+	u32 num_listner;
+	int state; /* Call state */ /* TODO remove this if not req*/
+};
+
+extern struct event_listner event;
+int auddev_register_evt_listner(u32 evt_id, u32 clnt_type, u32 clnt_id,
+		void (*listner)(u32 evt_id,
+			union auddev_evt_data *evt_payload,
+			void *private_data),
+		void *private_data);
+int auddev_unregister_evt_listner(u32 clnt_type, u32 clnt_id);
+void mixer_post_event(u32 evt_id, u32 dev_id);
+int snddev_request_freq(int *freq, u32 session_id, u32 clnt_type);
+int msm_snddev_request_freq(int *freq, u32 session_id, u32 clnt_type);
+
 #endif
