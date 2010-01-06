7c027c2163aa6f57c0b68cdae803c34e7f9760e0
Steve Muckle <smuckle@quicinc.com>
2010-01-06 12:16:44 -0800

    msm: io: clean up boot time IO mappings
    As more SoCs are added, having all the io map information in
    one file gets messy. There are also a couple mappings that
    can be removed.
    
    Change-Id: I7d99a95a5fb485789e42cbff4af4d0ab6e93a733
    Signed-off-by: Steve Muckle <smuckle@quicinc.com>

--- arch/arm/mach-msm/include/mach/msm_iomap.h
+++ arch/arm/mach-msm/include/mach/msm_iomap.h
@@ -1,7 +1,6 @@
-/* arch/arm/mach-msm/include/mach/msm_iomap.h
- *
+/*
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2008-2009, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2008-2010, Code Aurora Forum. All rights reserved.
  * Author: Brian Swetland <swetland@google.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -44,136 +43,12 @@
 #define IOMEM(x)	((void __force __iomem *)(x))
 #endif
 
-#define MSM_VIC_BASE          IOMEM(0xE0000000)
-#if defined(CONFIG_ARCH_QSD8X50)
-#define MSM_VIC_PHYS          0xAC000000
-#elif defined(CONFIG_ARCH_MSM7X30)
-#define MSM_VIC_PHYS          0xC0080000
-#else
-#define MSM_VIC_PHYS          0xC0000000
-#endif
-#define MSM_VIC_SIZE          SZ_4K
-
-#define MSM_CSR_BASE          IOMEM(0xE0001000)
-#if defined(CONFIG_ARCH_QSD8X50)
-#define MSM_CSR_PHYS          0xAC100000
-#else
-#define MSM_CSR_PHYS          0xC0100000
-#endif
-#define MSM_CSR_SIZE          SZ_4K
-
-#define MSM_TMR_PHYS          MSM_CSR_PHYS
-#define MSM_TMR_BASE          MSM_CSR_BASE
-#define MSM_TMR_SIZE          SZ_4K
-
-#define MSM_DMOV_BASE         IOMEM(0xE0002000)
 #if defined(CONFIG_ARCH_MSM7X30)
-#define MSM_DMOV_PHYS         0xAC400000
-#else
-#define MSM_DMOV_PHYS         0xA9700000
-#endif
-#define MSM_DMOV_SIZE         SZ_4K
-
-#define MSM_GPIO1_BASE        IOMEM(0xE0003000)
-#if defined(CONFIG_ARCH_QSD8X50)
-#define MSM_GPIO1_PHYS        0xA9000000
-#elif defined(CONFIG_ARCH_MSM7X30)
-#define MSM_GPIO1_PHYS        0xAC001000
-#else
-#define MSM_GPIO1_PHYS        0xA9200000
-#endif
-#define MSM_GPIO1_SIZE        SZ_4K
-
-#define MSM_GPIO2_BASE        IOMEM(0xE0004000)
-
-#if defined(CONFIG_ARCH_MSM7X30)
-#define MSM_GPIO2_PHYS        0xAC101000
+#include "msm_iomap-7x30.h"
 #elif defined(CONFIG_ARCH_QSD8X50)
-#define MSM_GPIO2_PHYS        0xA9100000
+#include "msm_iomap-8x50.h"
 #else
-#define MSM_GPIO2_PHYS        0xA9300000
+#include "msm_iomap-7xxx.h"
 #endif
-#define MSM_GPIO2_SIZE        SZ_4K
-
-#define MSM_CLK_CTL_BASE      IOMEM(0xE0005000)
-#if defined(CONFIG_ARCH_MSM7X30)
-#define MSM_CLK_CTL_PHYS      0xAB800000
-#else
-#define MSM_CLK_CTL_PHYS      0xA8600000
-#endif
-#define MSM_CLK_CTL_SIZE      SZ_4K
-
-#define MSM_L2CC_BASE         IOMEM(0xE0006000)
-#define MSM_L2CC_PHYS         0xC0400000
-#define MSM_L2CC_SIZE         SZ_4K
-
-#define MSM_SIRC_BASE         IOMEM(0xE1006000)
-#define MSM_SIRC_PHYS         0xAC200000
-#define MSM_SIRC_SIZE         SZ_4K
-
-#define MSM_SCPLL_BASE        IOMEM(0xE1007000)
-#define MSM_SCPLL_PHYS        0xA8800000
-#define MSM_SCPLL_SIZE        SZ_4K
-
-#define MSM_ACC_BASE          IOMEM(0xE0007000)
-#define MSM_ACC_PHYS          0xC0101000
-#define MSM_ACC_SIZE          SZ_4K
-
-#define MSM_SAW_BASE          IOMEM(0xE0008000)
-#define MSM_SAW_PHYS          0xC0102000
-#define MSM_SAW_SIZE          SZ_4K
-
-#define MSM_GCC_BASE	      IOMEM(0xE0009000)
-#define MSM_GCC_PHYS	      0xC0182000
-#define MSM_GCC_SIZE	      SZ_4K
-
-#define MSM_TCSR_BASE	      IOMEM(0xE000A000)
-#define MSM_TCSR_PHYS	      0xAB600000
-#define MSM_TCSR_SIZE	      SZ_4K
-
-#define MSM_SHARED_RAM_BASE   IOMEM(0xE0100000)
-#define MSM_SHARED_RAM_SIZE   SZ_1M
-
-#if defined(CONFIG_ARCH_MSM7X30)
-#define MSM_UART1_PHYS        0xACA00000
-#define MSM_UART2_PHYS        0xACB00000
-#define MSM_UART3_PHYS        0xACC00000
-#else
-#define MSM_UART1_PHYS        0xA9A00000
-#define MSM_UART2_PHYS        0xA9B00000
-#define MSM_UART3_PHYS        0xA9C00000
-#endif
-#define MSM_UART1_SIZE        SZ_4K
-#define MSM_UART2_SIZE        SZ_4K
-#define MSM_UART3_SIZE        SZ_4K
-
-#ifdef CONFIG_MSM_DEBUG_UART
-#define MSM_DEBUG_UART_BASE   0xE1000000
-#if CONFIG_MSM_DEBUG_UART == 1
-#define MSM_DEBUG_UART_PHYS   MSM_UART1_PHYS
-#elif CONFIG_MSM_DEBUG_UART == 2
-#define MSM_DEBUG_UART_PHYS   MSM_UART2_PHYS
-#elif CONFIG_MSM_DEBUG_UART == 3
-#define MSM_DEBUG_UART_PHYS   MSM_UART3_PHYS
-#endif
-#define MSM_DEBUG_UART_SIZE   SZ_4K
-#endif
-
-
-#define MSM_MDC_BASE	      IOMEM(0xE0200000)
-#define MSM_MDC_PHYS	      0xAA500000
-#define MSM_MDC_SIZE	      SZ_1M
-
-#define MSM_AD5_BASE          IOMEM(0xE0300000)
-#if defined(CONFIG_ARCH_MSM7X30)
-#define MSM_AD5_PHYS          0xA7000000
-#else
-#define MSM_AD5_PHYS          0xAC000000
-#endif
-#define MSM_AD5_SIZE          (SZ_1M*13)
-
-#define MSM_SSBI_BASE         IOMEM(0xE1004000)
-#define MSM_SSBI_PHYS         0xA8100000
-#define MSM_SSBI_SIZE         SZ_4K
 
 #endif
--- arch/arm/mach-msm/io.c
+++ arch/arm/mach-msm/io.c
@@ -132,8 +132,6 @@ static struct map_desc msm7x30_io_desc[] __initdata = {
 	MSM_DEVICE(GPIO1),
 	MSM_DEVICE(GPIO2),
 	MSM_DEVICE(CLK_CTL),
-	MSM_DEVICE(SIRC),
-	MSM_DEVICE(SCPLL),
 	MSM_DEVICE(AD5),
 	MSM_DEVICE(MDC),
 	MSM_DEVICE(ACC),
