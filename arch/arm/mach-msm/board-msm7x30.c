ab43fbc8de1b32bb945b4ba25789429327aade75
Naomi Luis <nluis@quicinc.com>
2010-02-26 10:29:22 -0800

    msm: Increase the size of /dev/pmem
    SurfaceFlinger requires atleast 23MB of pmem memory. Increase the size
    of /dev/pmem across all targets to accommodate this requirememt.
    
    Signed-off-by: Naomi Luis <nluis@quicinc.com>

--- arch/arm/mach-msm/board-msm7x30.c
+++ arch/arm/mach-msm/board-msm7x30.c
@@ -107,7 +107,7 @@
 #include <mach/msm_serial_hs.h>
 
 
-#define MSM_PMEM_SF_SIZE	0x1300000
+#define MSM_PMEM_SF_SIZE	0x1700000
 #define MSM_FB_SIZE		0x500000
 #define MSM_PMEM_GPU1_SIZE      0x1000000
 #define MSM_GPU_PHYS_SIZE       SZ_2M
