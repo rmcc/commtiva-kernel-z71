ddf9cc61e9b0c90de5cfb404ebf5df65b115a078
Stephen Boyd <sboyd@quicinc.com>
2010-03-18 19:39:47 -0700

    msm: clock-7x30: Support power rails in auto mode
    Power rails have two modes: manual and auto. When a rail is in
    manual mode, the driver assumes control of enabling and disabling
    the rail. When the rail is in auto mode, the clock which is
    closest related to the rail is used to determine if the rail
    should be turned on or off. For example, if the clock is on the
    rail is turned on as well.
    
    This works well when the clock is proc_comm controlled since the
    modem can tell when a clock is on or off. When the clocks are
    locally owned, the modem will do nothing for rails in auto mode
    since it never receives proc_comm requests for the clocks the
    rails are related to.
    
    Therefore call into the power rail code from the local clock code
    and enable or disable the rails for the associated clock as long
    as the rail is not manually controlled. This simulates auto mode
    for locally controlled clocks.
    
    Change-Id: I62061306b93fb2f6b2e77915581887f520a76c8b
    Signed-off-by: Stephen Boyd <sboyd@quicinc.com>

--- arch/arm/mach-msm/clock-7x30.h
+++ arch/arm/mach-msm/clock-7x30.h
@@ -148,6 +148,8 @@ static inline int clk_7x30_init(void) { return 0; }
 void pll_enable(uint32_t pll);
 void pll_disable(uint32_t pll);
 
+extern int internal_pwr_rail_ctl_auto(unsigned rail_id, bool enable);
+
 #define CLK_7X30(clk_name, clk_id, clk_dev, clk_flags) {	\
 	.name = clk_name, \
 	.id = L_7X30_##clk_id, \
