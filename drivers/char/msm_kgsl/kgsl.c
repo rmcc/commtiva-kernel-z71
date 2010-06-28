0621b991a2ecfe81c4bbc51aa689c95835560836
Jon Ashburn <jashburn@codeaurora.org>
2010-06-28 13:37:12 -0600

    msm: kgsl: Correct context ID and mask usage for 2D core
    Context creation and destruction did not handle high context counts
    correctly. This lead to occasional failures to create a context in
    OpenVG apps. This patch fixes this issue.
    
    Change-Id: I7ea79d767134d84e118851fb62af5070c62aa4d2
    Signed-off-by: Jon Ashburn <jashburn@codeaurora.org>
    Signed-off-by: Vasantha Kumar <vasant@codeaurora.org>

--- drivers/char/msm_kgsl/kgsl.c
+++ drivers/char/msm_kgsl/kgsl.c
@@ -812,9 +812,7 @@ static long kgsl_ioctl_rb_issueibcmds(struct kgsl_file_private *private,
 					     param.flags);
 	} else if (param.device_id == KGSL_DEVICE_G12) {
 		KGSL_G12_PRE_HWACCESS();
-		if (param.drawctxt_id >= KGSL_CONTEXT_MAX ||
-			(private->g12_ctxt_id_mask & 1 <<
-				(param.drawctxt_id - 1)) == 0) {
+		if ((private->g12_ctxt_id_mask & 1 << param.drawctxt_id) == 0) {
 
 			result = -EINVAL;
 			KGSL_DRV_ERR("invalid drawctxt drawctxt_id %d\n",
@@ -963,9 +961,8 @@ static long kgsl_ioctl_drawctxt_create(struct kgsl_file_private *private,
 	} else if (param.device_id == KGSL_DEVICE_G12) {
 		KGSL_G12_PRE_HWACCESS();
 		result = kgsl_g12_drawctxt_create(&kgsl_driver.g12_device,
-					0,
-					&param.drawctxt_id,
-					param.flags);
+					private->g12_ctxt_id_mask,
+					&param.drawctxt_id);
 		if (result != 0)
 			goto done;
 
@@ -976,7 +973,7 @@ static long kgsl_ioctl_drawctxt_create(struct kgsl_file_private *private,
 
 		/* if KGSL_CONTEXT_MAX is more than 16,
 		then linked list should be used for them */
-		private->g12_ctxt_id_mask |= 1 << (param.drawctxt_id - 1);
+		private->g12_ctxt_id_mask |= 1 << param.drawctxt_id;
 	} else {
 		result = -EINVAL;
 	}
@@ -1011,9 +1008,7 @@ static long kgsl_ioctl_drawctxt_destroy(struct kgsl_file_private *private,
 					~(1 << param.drawctxt_id);
 	} else if (param.device_id == KGSL_DEVICE_G12) {
 		KGSL_G12_PRE_HWACCESS();
-		if (param.drawctxt_id >= KGSL_CONTEXT_MAX
-				|| (private->g12_ctxt_id_mask & 1 <<
-				(param.drawctxt_id-1)) == 0) {
+		if ((private->g12_ctxt_id_mask & 1 << param.drawctxt_id) == 0) {
 			result = -EINVAL;
 			goto done;
 		}
@@ -1022,7 +1017,7 @@ static long kgsl_ioctl_drawctxt_destroy(struct kgsl_file_private *private,
 					param.drawctxt_id);
 		if (result == 0)
 			private->g12_ctxt_id_mask &= ~(1 <<
-						(param.drawctxt_id-1));
+						param.drawctxt_id);
 
 		kgsl_runpending(&kgsl_driver.g12_device);
 	} else {
