46625ad9c980e6a0803a899a54637c3bc801c76f
Jaikumar Ganesh <jaikumar@google.com>
2010-11-09 19:07:45 -0800

    Bluetooth: Add BT_POWER L2CAP socket option.
    Add BT_POWER socket option used to control the power
    characteristics of the underlying ACL link. When the remote end
    has put the link in sniff mode and the host stack wants to send
    data we need need to explicitly exit sniff mode to work well with
    certain devices (For example, A2DP on Plantronics Voyager 855).
    However, this causes problems with HID devices.
    
    Hence, moving into active mode when sending data, irrespective
    of who set the sniff mode has been made as a socket option. By
    default, we will move into active mode. HID devices can set the
    L2CAP socket option to prevent this from happening.
    
    Currently, this has been implemented for L2CAP sockets. This has been
    tested with incoming and outgoing L2CAP sockets for HID and A2DP.
    
    Based on discussions on linux-bluetooth and patches submitted by
    Andrei Emeltchenko.
    Signed-off-by: Jaikumar Ganesh <jaikumar@google.com>
    (cherry picked from commit a01f107f42a3aee9571a5228e6143c2bcd7b76a7)
    
    Change-Id: I52db89e32b0eb012ebd90baf0d47d819e1569656
    
    Signed-off-by: Srinivas Krovvidi <skrovvid@codeaurora.org>

--- net/bluetooth/l2cap_core.c
+++ net/bluetooth/l2cap_core.c
@@ -709,6 +709,7 @@ static inline void l2cap_send_cmd(struct l2cap_conn *conn, u8 ident, u8 code, u1
 	else
 		flags = ACL_START;
 
+	bt_cb(skb)->force_active = 1;
 	hci_send_acl(conn->hcon, NULL, skb, flags);
 }
 
@@ -1220,6 +1221,7 @@ static void l2cap_sock_init(struct sock *sk, struct sock *parent)
 		pi->role_switch = l2cap_pi(parent)->role_switch;
 		pi->force_reliable = l2cap_pi(parent)->force_reliable;
 		pi->flushable = l2cap_pi(parent)->flushable;
+		pi->force_active = l2cap_pi(parent)->force_active;
 		pi->amp_pref = l2cap_pi(parent)->amp_pref;
 	} else {
 		pi->imtu = L2CAP_DEFAULT_MTU;
@@ -1238,6 +1240,7 @@ static void l2cap_sock_init(struct sock *sk, struct sock *parent)
 		pi->role_switch = 0;
 		pi->force_reliable = 0;
 		pi->flushable = 0;
+		pi->force_active = 1;
 		pi->amp_pref = BT_AMP_POLICY_REQUIRE_BR_EDR;
 	}
 
@@ -1804,6 +1807,7 @@ static inline void l2cap_do_send(struct sock *sk, struct sk_buff *skb)
 			flags = ACL_START_NO_FLUSH;
 		else
 			flags = ACL_START;
+		bt_cb(skb)->force_active = pi->force_active;
 
 		hci_send_acl(pi->conn->hcon, NULL, skb, flags);
 	}
@@ -3379,6 +3383,7 @@ static int l2cap_sock_setsockopt(struct socket *sock, int level, int optname, ch
 {
 	struct sock *sk = sock->sk;
 	struct bt_security sec;
+	struct bt_power pwr;
 	int len, err = 0;
 	u32 opt;
 
@@ -3429,6 +3434,23 @@ static int l2cap_sock_setsockopt(struct socket *sock, int level, int optname, ch
 		}
 
 		bt_sk(sk)->defer_setup = opt;
+		break;
+    case BT_POWER:
+		if (sk->sk_type != SOCK_SEQPACKET && sk->sk_type != SOCK_STREAM
+			&& sk->sk_type != SOCK_RAW) {
+			err = -EINVAL;
+			break;
+		}
+
+		pwr.force_active = 1;
+
+		len = min_t(unsigned int, sizeof(pwr), optlen);
+		if (copy_from_user((char *) &pwr, optval, len)) {
+			err = -EFAULT;
+			break;
+		}
+		BT_DBG("BT_POWER setting %d", pwr.force_active);
+		l2cap_pi(sk)->force_active = pwr.force_active;
 		break;
 
 	case BT_AMP_POLICY:
@@ -3556,6 +3578,7 @@ static int l2cap_sock_getsockopt(struct socket *sock, int level, int optname, ch
 {
 	struct sock *sk = sock->sk;
 	struct bt_security sec;
+	struct bt_power pwr;
 	int len, err = 0;
 
 	BT_DBG("sk %p", sk);
@@ -3597,7 +3620,20 @@ static int l2cap_sock_getsockopt(struct socket *sock, int level, int optname, ch
 			err = -EFAULT;
 
 		break;
+	case BT_POWER:
+		if (sk->sk_type != SOCK_SEQPACKET && sk->sk_type != SOCK_STREAM
+				&& sk->sk_type != SOCK_RAW) {
+			err = -EINVAL;
+			break;
+		}
 
+		pwr.force_active = l2cap_pi(sk)->force_active;
+
+		len = min_t(unsigned int, len, sizeof(pwr));
+		if (copy_to_user(optval, (char *) &pwr, len))
+			err = -EFAULT;
+
+		break;
 	case BT_AMP_POLICY:
 
 		if (put_user(l2cap_pi(sk)->amp_pref, (u32 __user *) optval))
