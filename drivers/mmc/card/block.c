57073529d69b5fc8719e58a09708f9406692045f
Ricardo Cerqueira <github@cerqueira.org>
2010-11-14 01:11:40 +0000

    mmc: Reintroduce FIH hack to mmc block code
    I suspect some of the SD card issues are due to the lack of this,
    but since I can't reproduce them there's no way to be sure

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -378,6 +378,7 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 		 * programming mode even when things go wrong.
 		 */
 		if (brq.cmd.error || brq.data.error || brq.stop.error) {
+#ifndef CONFIG_FIH_FXX
 			if (brq.data.blocks > 1 && rq_data_dir(req) == READ) {
 				/* Redo read one sector at a time */
 				printk(KERN_WARNING "%s: retrying using single "
@@ -385,6 +386,7 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 				disable_multi = 1;
 				continue;
 			}
+#endif
 			status = get_card_status(card, req);
 		} else if (disable_multi == 1) {
 			disable_multi = 0;
@@ -426,7 +428,11 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 				if (err) {
 					printk(KERN_ERR "%s: error %d requesting status\n",
 					       req->rq_disk->disk_name, err);
+#ifdef CONFIG_FIH_FXX
+					break;
+#else
 					goto cmd_err;
+#endif
 				}
 				/*
 				 * Some cards mishandle the status bits,
@@ -446,6 +452,12 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 		}
 
 		if (brq.cmd.error || brq.stop.error || brq.data.error) {
+#ifdef CONFIG_FIH_FXX
+			spin_lock_irq(&md->lock);
+			ret = __blk_end_request(req, -EIO, brq.data.blksz);
+			spin_unlock_irq(&md->lock);
+			continue;
+#else
 			if (rq_data_dir(req) == READ) {
 				/*
 				 * After an error, we redo I/O one sector at a
@@ -457,6 +469,7 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 				spin_unlock_irq(&md->lock);
 				continue;
 			}
+#endif
 			goto cmd_err;
 		}
 
