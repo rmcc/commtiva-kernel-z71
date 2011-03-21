5991fcb400aa0617255fed7caef2b2dd15ca1c53
Ricardo Cerqueira <github@cerqueira.org>
2011-03-21 23:28:52 +0000

    lcdc_innolux: Shut down lcdc clocks on standby

--- drivers/video/msm/lcdc_innolux.c
+++ drivers/video/msm/lcdc_innolux.c
@@ -704,7 +704,7 @@ static int create_lcdspi_proc_file(void)
     return 0;
 }
 extern int fih_lcm_is_mddi_type(void);
-//extern void lcdc_clk_off(void);
+extern void lcdc_clk_off(void);
 static int __init lcdc_innolux_init(void)
 {
 	int ret;
@@ -712,7 +712,7 @@ static int __init lcdc_innolux_init(void)
 	
     
     if(fih_lcm_is_mddi_type()){
-        //lcdc_clk_off();        
+        lcdc_clk_off();        
         ret=-ENODEV;
         goto err;
     }
