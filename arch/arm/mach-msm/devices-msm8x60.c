863b40560ebae9e2a98924515d267fc6ae8e835b
Shruthi Krishna <skrish@codeaurora.org>
2011-05-26 14:21:50 -0700

    msm: kgsl: Idle calc
    This reverts commit debd216b762a87da0faf0eb4f991ed10c7a052cc.
    
    Change-Id: I53a8a4fd648f199c577b2959711b22d90c4ea5c8
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>
    Signed-off-by: Raj Kushwaha <rajk@codeaurora.org>

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -465,12 +465,21 @@ static struct msm_bus_vectors grp3d_init_vectors[] = {
 	},
 };
 
-static struct msm_bus_vectors grp3d_nominal_vectors[] = {
+static struct msm_bus_vectors grp3d_nominal_low_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = 200800000U,
+		.ib = 1397000000U,
+	},
+};
+
+static struct msm_bus_vectors grp3d_nominal_high_vectors[] = {
+	{
+		.src = MSM_BUS_MASTER_GRAPHICS_3D,
+		.dst = MSM_BUS_SLAVE_EBI_CH0,
+		.ab = 0,
+		.ib = 2008000000U,
 	},
 };
 
@@ -479,7 +488,7 @@ static struct msm_bus_vectors grp3d_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = 2008000000U,
+		.ib = 2484000000U,
 	},
 };
 
@@ -489,8 +498,12 @@ static struct msm_bus_paths grp3d_bus_scale_usecases[] = {
 		grp3d_init_vectors,
 	},
 	{
-		ARRAY_SIZE(grp3d_nominal_vectors),
-		grp3d_nominal_vectors,
+		ARRAY_SIZE(grp3d_nominal_low_vectors),
+		grp3d_nominal_low_vectors,
+	},
+	{
+		ARRAY_SIZE(grp3d_nominal_high_vectors),
+		grp3d_nominal_high_vectors,
 	},
 	{
 		ARRAY_SIZE(grp3d_max_vectors),
@@ -519,7 +532,7 @@ static struct msm_bus_vectors grp2d0_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = 248000000,
+		.ib = 1397000000U,
 	},
 };
 
@@ -554,7 +567,7 @@ static struct msm_bus_vectors grp2d1_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = 248000000,
+		.ib = 1397000000U,
 	},
 };
 
@@ -612,23 +625,28 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		.pwrlevel = {
 			{
 				.gpu_freq = 266667000,
-				.bus_freq = 2,
+				.bus_freq = 3,
 			},
 			{
 				.gpu_freq = 228571000,
+				.bus_freq = 2,
+			},
+			{
+				.gpu_freq = 200000000,
 				.bus_freq = 1,
 			},
 			{
-				.gpu_freq = 266667000,
+				.gpu_freq = 27000000,
 				.bus_freq = 0,
 			},
 		},
 		.init_level = 0,
-		.num_levels = 3,
+		.num_levels = 4,
 		.set_grp_async = NULL,
 		.idle_timeout = HZ/5,
 #ifdef CONFIG_MSM_BUS_SCALING
 		.nap_allowed = true,
+		.idle_pass = true,
 #endif
 		.pwrrail_first = true,
 	},
@@ -677,11 +695,11 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.pwr_data = {
 		.pwrlevel = {
 			{
-				.gpu_freq = 228571000,
+				.gpu_freq = 200000000,
 				.bus_freq = 1,
 			},
 			{
-				.gpu_freq = 228571000,
+				.gpu_freq = 200000000,
 				.bus_freq = 0,
 			},
 		},
@@ -735,11 +753,11 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.pwr_data = {
 		.pwrlevel = {
 			{
-				.gpu_freq = 228571000,
+				.gpu_freq = 200000000,
 				.bus_freq = 1,
 			},
 			{
-				.gpu_freq = 228571000,
+				.gpu_freq = 200000000,
 				.bus_freq = 0,
 			},
 		},
--- drivers/gpu/msm/kgsl_g12.c
+++ drivers/gpu/msm/kgsl_g12.c
@@ -738,6 +738,11 @@ static long kgsl_g12_ioctl(struct kgsl_device_private *dev_priv,
 
 }
 
+static unsigned int kgsl_g12_idle_calc(struct kgsl_device *device)
+{
+	return device->pwrctrl.time;
+}
+
 static void kgsl_g12_getfunctable(struct kgsl_functable *ftbl)
 {
 
@@ -763,6 +768,7 @@ static void kgsl_g12_getfunctable(struct kgsl_functable *ftbl)
 	ftbl->device_ioctl = kgsl_g12_ioctl;
 	ftbl->device_setup_pt = kgsl_g12_setup_pt;
 	ftbl->device_cleanup_pt = kgsl_g12_cleanup_pt;
+	ftbl->device_idle_calc = kgsl_g12_idle_calc;
 }
 
 static struct platform_device_id kgsl_2d_id_table[] = {
