cfd0baafd55ceb6273b6edb4e1ea6791ec256a42
Michael Bohan <mbohan@codeaurora.org>
2011-05-31 16:49:22 -0700

    board-msm8660: Add batt_alarm device to board file
    This is used to support msm_charger.
    
    Signed-off-by: Michael Bohan <mbohan@codeaurora.org>
    (cherry picked from commit 3ba54696bfd83e24a94e1be3752c97b35d805ac4)
    
    Change-Id: Ibb55865ea035dbcc60892325c402c83ad25cb95e
    Signed-off-by: Sunil Joseph <sunilj@codeaurora.org>
    Signed-off-by: Michael Bohan <mbohan@codeaurora.org>

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -24,6 +24,7 @@
 #include <linux/mfd/pmic8058.h>
 
 #include <linux/input/pmic8058-keypad.h>
+#include <linux/pmic8058-batt-alarm.h>
 #include <linux/pmic8058-pwrkey.h>
 #include <linux/pmic8058-vibrator.h>
 #include <linux/leds.h>
@@ -5667,19 +5668,27 @@ static struct pmic8058_leds_platform_data pm8058_fluid_flash_leds_data = {
 };
 
 static struct resource resources_temp_alarm[] = {
-       {
+	{
 		.start  = PM8058_TEMP_ALARM_IRQ(PM8058_IRQ_BASE),
 		.end    = PM8058_TEMP_ALARM_IRQ(PM8058_IRQ_BASE),
 		.flags  = IORESOURCE_IRQ,
-       },
+	},
 };
 
 static struct resource resources_pm8058_misc[] = {
-       {
+	{
 		.start  = PM8058_OSCHALT_IRQ(PM8058_IRQ_BASE),
 		.end    = PM8058_OSCHALT_IRQ(PM8058_IRQ_BASE),
 		.flags  = IORESOURCE_IRQ,
-       },
+	},
+};
+
+static struct resource resources_pm8058_batt_alarm[] = {
+	{
+		.start  = PM8058_BATT_ALARM_IRQ(PM8058_IRQ_BASE),
+		.end    = PM8058_BATT_ALARM_IRQ(PM8058_IRQ_BASE),
+		.flags  = IORESOURCE_IRQ,
+	},
 };
 
 #define PM8058_SUBDEV_KPD 0
@@ -5781,6 +5790,11 @@ static struct mfd_cell pm8058_subdevs[] = {
 		.id = -1,
 		.num_resources  = ARRAY_SIZE(resources_pm8058_misc),
 		.resources      = resources_pm8058_misc,
+	},
+	{	.name = "pm8058-batt-alarm",
+		.id		= -1,
+		.num_resources  = ARRAY_SIZE(resources_pm8058_batt_alarm),
+		.resources      = resources_pm8058_batt_alarm,
 	},
 };
 
