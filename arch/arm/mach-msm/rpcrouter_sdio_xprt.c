12647aca939b97cb8ea9bfb22375d5eb7d1657a0
Karthikeyan Ramasubramanian <kramasub@codeaurora.org>
2011-06-24 17:04:19 -0600

    msm: rpc: Add wakelock in rpcrouter's sdio_xprt
    Adding the wakelock to sdio_xprt to avoid the APPs from getting into
    power collapse when there is data for the RPC Router.
    
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@codeaurora.org>
    (cherry picked from commit 479c75b1789113a3576129c475778cebdf8ede32)
    
    Change-Id: I0d4b64b0db744952d1ed9109b4c442e89235a571
    Signed-off-by: Raj Kushwaha <rajk@codeaurora.org>

--- arch/arm/mach-msm/rpcrouter_sdio_xprt.c
+++ arch/arm/mach-msm/rpcrouter_sdio_xprt.c
@@ -80,6 +80,8 @@ struct sdio_xprt {
 
 	struct list_head free_list;
 	spinlock_t free_list_lock;
+
+	struct wake_lock read_wakelock;
 };
 
 struct rpcrouter_sdio_xprt {
@@ -150,6 +152,7 @@ static void free_sdio_xprt(struct sdio_xprt *chnl)
 	}
 	num_rx_bufs = 0;
 	spin_unlock_irqrestore(&chnl->read_list_lock, flags);
+	wake_unlock(&chnl->read_wakelock);
 }
 
 static struct sdio_buf_struct *alloc_from_free_list(struct sdio_xprt *chnl)
@@ -247,6 +250,9 @@ static int rpcrouter_sdio_remote_read(void *data, uint32_t len)
 		num_rx_bufs--;
 		return_to_free_list(sdio_remote_xprt.channel, buf);
 	}
+
+	if (list_empty(&sdio_remote_xprt.channel->read_list))
+		wake_unlock(&sdio_remote_xprt.channel->read_wakelock);
 	spin_unlock_irqrestore(&sdio_remote_xprt.channel->read_list_lock,
 				flags);
 	return len;
@@ -462,6 +468,7 @@ static void sdio_xprt_read_data(struct work_struct *work)
 		num_rx_bufs++;
 		spin_unlock_irqrestore(
 			&sdio_remote_xprt.channel->read_list_lock, flags);
+		wake_lock(&sdio_remote_xprt.channel->read_wakelock);
 	}
 
 	if (!modem_reset && !list_empty(&sdio_remote_xprt.channel->read_list))
@@ -508,6 +515,8 @@ static int allocate_sdio_xprt(struct sdio_xprt **sdio_xprt_chnl)
 		INIT_LIST_HEAD(&chnl->write_list);
 		INIT_LIST_HEAD(&chnl->read_list);
 		INIT_LIST_HEAD(&chnl->free_list);
+		wake_lock_init(&chnl->read_wakelock,
+				WAKE_LOCK_SUSPEND, "rpc_sdio_xprt_read");
 	} else {
 		chnl = *sdio_xprt_chnl;
 	}
@@ -537,6 +546,7 @@ alloc_failure:
 		kfree(buf);
 	}
 	spin_unlock_irqrestore(&chnl->free_list_lock, flags);
+	wake_lock_destroy(&chnl->read_wakelock);
 
 	kfree(chnl);
 	*sdio_xprt_chnl = NULL;
