85200a4e8aabf77e3df2bbb0d50b359ee54ba402
kuogee hsieh <khsieh@codeaurora.org>
2011-06-27 11:09:23 -0700

    msm_fb: display: add Novatek dsi panel backlight support
    Add MIPI led_pwm1, led_pwm2 and led_pwm3 dcs commands to control
    Novatek panel backlight through dcs command.
    
    CRs-fixed: 285850
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    (cherry picked from commit 098325372eb8c3df9e841de9502c4bd7dcbbf5d5)
    
    Change-Id: Ib5b4a9d0be4c03eb0d6525ceb1d5cdd04e970a96
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- drivers/video/msm/mipi_novatek_cmd_qhd_pt.c
+++ drivers/video/msm/mipi_novatek_cmd_qhd_pt.c
@@ -64,7 +64,7 @@ static int __init mipi_cmd_novatek_blue_qhd_pt_init(void)
 	pinfo.lcdc.border_clr = 0;	/* blk */
 	pinfo.lcdc.underflow_clr = 0xff;	/* blue */
 	pinfo.lcdc.hsync_skew = 0;
-	pinfo.bl_max = 15;
+	pinfo.bl_max = 255;
 	pinfo.bl_min = 1;
 	pinfo.fb_num = 2;
 	pinfo.clk_rate = 454000000;
